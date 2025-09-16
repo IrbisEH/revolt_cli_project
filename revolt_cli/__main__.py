d2fee1f657db97dc12c65708f4c0cc58f8db706d
ehristenko <hristenko.evgeniy@yandex.ru>
2025-09-16 12:48:40 +0300

    add debug func

--- revolt_cli/__main__.py
+++ revolt_cli/__main__.py
@@ -5,5 +5,12 @@ def main():
     cli = CliManager()
     cli.run()
 
+
+def debug_main():
+    import pydevd_pycharm
+    pydevd_pycharm.settrace('192.168.1.49', port=9000, stdoutToServer=True, stderrToServer=True)
+    main()
+
+
 if __name__ == '__main__':
     main()
